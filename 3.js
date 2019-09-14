9368b34b9a2d3d9013696edd0a1d239a8c76ef9b
Andhika Ilham <55274823+phasrock66@users.noreply.github.com>
2019-09-14 19:48:47 +0700

    Soalan 3.0

---
+++ 3.js
@@ -0,0 +1,20 @@
+function print_picture(size) {
+  const star = ' @ ';
+  const line = ' = ';
+
+  // bukan bilangan ganjil
+  if(size % 2 == 0) return;
+
+  // buat header berada di tengah
+  console.log(' '.repeat(size / 2) + '--- panjang ---' + ' '.repeat(size / 2));
+
+  for(var row = 0;  < size; row++) {
+    // bintang di tengah
+    if(Math.floor(size / 2) === row) {
+      console.log(star.repeat(size));
+    }else {
+      // bintang - garis - bintang
+      console.log(star+line.repeat(size - 2)+star);
+    }
+  }
+}
