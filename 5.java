7d7ec1799cb2444b56c8cae9ceb19524622ad9db
Andhika Ilham <55274823+phasrock66@users.noreply.github.com>
2019-09-14 20:23:40 +0700

    Soalan 5.0

---
+++ 5.java
@@ -0,0 +1,11 @@
+public static void main(String[] args) {
+        char karakter= '*' ;
+        
+        System.out.println(karakter);
+        System.out.println("     "+karakter);
+        System.out.println("            "+karakter);
+        System.out.println("                   "+karakter);
+        System.out.println("                          "+karakter);
+        System.out.println("                                 "+karakter);
+    
+    }
